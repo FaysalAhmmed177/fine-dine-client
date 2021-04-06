758908ca4622d9f18fff02b65bcff3d7f6a3ef7c
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-04-07 00:14:20 +0600

    starting fine-dine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import NotMatch from './Components/NotMatch/NotMatch';
+import { createContext } from 'react';
+import { useState } from 'react';
+
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Router path="*">
+            <NotMatch />
+          </Router>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/Components/NotMatch/NotMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotMatch = () => {
+    return (
+        <div>
+            <h1 className="text-danger text-center mt-5">404 Error</h1>
+        </div>
+    );
+};
+
+export default NotMatch;
